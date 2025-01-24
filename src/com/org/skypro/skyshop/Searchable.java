49fe88eb56126e79696f96ae5fbf38340f3965d6
origin <viktoryaa2000@mail.ru>
2025-01-24 16:50:27 +0300

    commit 2.4

---
+++ src/com/org/skypro/skyshop/Searchable.java
@@ -0,0 +1,11 @@
+package com.org.skypro.skyshop;
+
+public interface Searchable {
+    String getSearchTerm();
+    String getContentType();
+    String getName();
+
+    default String getStringRepresentation() {
+        return getName() + " - " + getContentType();
+    }
+}
