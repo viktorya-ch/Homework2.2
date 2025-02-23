2ba848285624e7bf7d78e8ac1aa2641ef2fd7eb2
origin <viktoryaa2000@mail.ru>
2025-02-23 15:51:21 +0300

    commit 2.6

--- src/com/org/skypro/skyshop/searchengine/SearchEngine.java
+++ src/com/org/skypro/skyshop/searchengine/SearchEngine.java
@@ -1,5 +1,6 @@
 package com.org.skypro.skyshop.searchengine;
 
+import com.org.skypro.skyshop.article.Article;
 import com.org.skypro.skyshop.searchable.Searchable;
 
 import java.util.ArrayList;
@@ -13,67 +14,59 @@ public class SearchEngine {
         searchables = new ArrayList<>();
     }
 
-    public Searchable[] elements;
-    private int count;
-
-    public SearchEngine(int size) {
-        this.elements = new Searchable[size];
-        this.count = 0;
+    public void addSearchable(Searchable searchable) {
+        searchables.add(searchable);
     }
 
-    public void add(Searchable type) {
-        if (count < elements.length) {
-            elements[count] = type;
-            count++;
-        } else {
-            System.out.println(" Массив полный. Невозможно добавить новый элемент. ");
-        }
-    }
-
-    public Searchable[] search(String searchTerm) {
-        Searchable[] results = new Searchable[5];
-        int resultCount = 0;
-
-        for (int i = 0; i < count; i++) {
-            if (elements[i] != null) {
-                results[resultCount] = elements[i];
-                resultCount++;
-            }
-            if (resultCount == 5) {
-                break;
-            }
-        }
-        return results;
-    }
-
-
-    public int countOccurrences(String str, String substring) {
+    private int countOccurrences(String str, String substring) {
         int count = 0;
         int index = 0;
         while ((index = str.indexOf(substring, index)) != -1) {
-
             count++;
             index += substring.length();
         }
         return count;
     }
 
-    public Searchable findSuitableObject(String search) throws BestResultNotFound {
-        int maxCount = -1;
-        Searchable suitableObject = null;
-        for (Searchable searchable : elements) {
+    public List<Searchable> findAllObjects(String search) {
+        List<Searchable> objects = new ArrayList<>();
+        for (Searchable searchable : searchables) {
             String term = searchable.getSearchTerm();
             int count = countOccurrences(term, search);
-            if (count > maxCount) {
-                maxCount = count;
-                suitableObject = searchable;
+            if (count > 0) {
+                objects.add(searchable);
             }
         }
-        if (suitableObject == null) {
-            throw new BestResultNotFound(" Объект не найден для запроса: " + search);
-        }
-        return suitableObject;
+        return objects;
     }
-
-
 }
+
+
+//    public Searchable[] elements;
+//    private int count;
+//
+//    public SearchEngine(int size) {
+//        this.elements = new Searchable[size];
+//        this.count = 0;
+//    }
+
+
+//
+//    public Searchable[] search(String searchTerm) {
+//        Searchable[] results = new Searchable[5];
+//        int resultCount = 0;
+//
+//        for (int i = 0; i < count; i++) {
+//            if (elements[i] != null) {
+//                results[resultCount] = elements[i];
+//                resultCount++;
+//            }
+//            if (resultCount == 5) {
+//                break;
+//            }
+//        }
+//        return results;
+//    }
+//
+//
+//}
